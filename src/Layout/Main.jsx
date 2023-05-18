6c557fec9af3d36c1ccf955f3ee991920f7d5c89
Wasey Rahman <wasey123435@gmail.com>
2023-05-18 15:32:51 +0600

    NavigationBar

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../pages/Shared/Footer/Footer';
+import NavigationBar from '../pages/Shared/NavigationBar/NavigationBar';
 
 
 const Main = () => {
     return (
         <div>
-            
+            <NavigationBar></NavigationBar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>

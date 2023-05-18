25914e9cd30fecd2d732c3f2a1b34e51580dda90
Wasey Rahman <wasey123435@gmail.com>
2023-05-18 17:03:53 +0600

    Updated LogIn

--- src/Layout/LogIn/LogIn.jsx
+++ src/Layout/LogIn/LogIn.jsx
@@ -1,34 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const LogIn = () => {
-    const handleGoogleSignIn = () => {
-        // Implement Google Sign-in logic here
-        // This function will be triggered when the user clicks on the Google Sign-in button
-      };
-    return (
-        <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    
-    <div className=" card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 mt-1">
-      <div className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
-          
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
-        </div>
-        <div className="form-control mt-4">
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleLogin = () => {
+    // Implement login logic here
+    // Validate email and password
+    if (email === '') {
+      setErrorMessage('Please enter your email.');
+    } else if (password === '') {
+      setErrorMessage('Please enter your password.');
+    } else {
+      // Perform login action
+      // ...
+    }
+  };
+
+  const handleGoogleSignIn = () => {
+    // Implement Google Sign-in logic here
+    // This function will be triggered when the user clicks on the Google Sign-in button
+  };
+
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 mt-1">
+          <div className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="email"
+                className="input input-bordered"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            {errorMessage && <p className="text-error mt-2">{errorMessage}</p>}
+            <div className="form-control mt-6">
+              <button className="btn btn-primary" onClick={handleLogin}>
+                Login
+              </button>
+            </div>
+            <div className="form-control mt-4">
               <button className="btn btn-neutral" onClick={handleGoogleSignIn}>
                 Google Sign-in
               </button>
@@ -41,11 +72,11 @@ const LogIn = () => {
                 </Link>
               </p>
             </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default LogIn;

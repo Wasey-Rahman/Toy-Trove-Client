e2e0ad79965838d89b752881386532db21dbe0a7
Wasey Rahman <wasey123435@gmail.com>
2023-05-19 12:46:22 +0600

    Firebase config

--- src/pages/Shared/NavigationBar/NavigationBar.jsx
+++ src/pages/Shared/NavigationBar/NavigationBar.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from './Toy-Trove.png';
 
 const NavigationBar = () => {
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false); // Added state for dropdown visibility
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   // Sample login state and user data
   const [isLoggedIn, setIsLoggedIn] = React.useState(true); // Update this based on your login logic
   const username = 'John Doe';
@@ -16,13 +23,20 @@ const NavigationBar = () => {
     <div className="navbar bg-base-100">
       <div className="navbar-start">
         <div className="dropdown">
+        <label tabIndex={0} className="btn btn-ghost lg:hidden" onClick={toggleDropdown}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+            </svg>
+          </label>
+          {isDropdownOpen && (
           <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a>Home</a></li>
+            <li><Link to="/"><span>Home</span></Link></li>
             <li tabIndex={0}>
-              <a className="justify-between">All Toys</a>
+             <Link to="/AllToys"className="justify-between">All Toys</Link>
             </li>
-            <li><a>Blogs</a></li>
+            <li><Link to="/Blogs">Blogs</Link></li>
           </ul>
+          )}
         </div>
         <Link to="/" className="normal-case text-xl">
           <img className="w-16 me-4" src={logo} alt="" />
